f3a327c4a445ce815367e202e6caa87292dd3804
Mathias Larsen <mnla@itu.dk>
2026-02-06 11:56:32 +0100

    baseline

--- AndroidProjects/X9/app/build.gradle.kts
+++ AndroidProjects/X9/app/build.gradle.kts
@@ -49,7 +49,8 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.material3)
+    implementation(libs.androidx.compose.material3)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     debugImplementation(libs.androidx.compose.ui.tooling)
+
 }
