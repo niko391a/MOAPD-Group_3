b669cc0eb4310b837446244f1485ff3acf609d49
Nikolaj <nals@itu.dk>
2026-02-20 12:01:26 +0100

    Fresh project
    New jetpack compose project, in which old features will be re-added

---
+++ X9/app/src/main/java/dk/itu/moapd/x9/mnla_nals/MainActivity.kt
@@ -0,0 +1,47 @@
+package dk.itu.moapd.x9.mnla_nals
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dk.itu.moapd.x9.mnla_nals.ui.theme.X9Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            X9Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    X9Theme {
+        Greeting("Android")
+    }
+}
