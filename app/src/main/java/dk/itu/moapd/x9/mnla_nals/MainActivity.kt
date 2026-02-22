6b642ec8437537934350913138f000f8d2d4ff00
Nikolaj <nals@itu.dk>
2026-02-22 13:19:12 +0100

    Added oncreate log statement
    Co-Authored-By: Mathias Niklas Larsen <143803020+DragonFlyersx@users.noreply.github.com>

--- app/src/main/java/dk/itu/moapd/x9/mnla_nals/MainActivity.kt
+++ app/src/main/java/dk/itu/moapd/x9/mnla_nals/MainActivity.kt
@@ -17,19 +17,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import dk.itu.moapd.x9.mnla_nals.ui.theme.X9Theme
+import android.util.Log
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
+        Log.d("Info","On Create was called for MainActivity")
 //        // Start the CreateReportActivity directly
 //           Legacy since we now use compose
 //        val intent = Intent(this, CreateReportActivity::class.java)
