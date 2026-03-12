77a107079dd7018df5eeff9f09f59f2eb3f3917a
Nikolaj <nals@itu.dk>
2026-03-12 16:36:25 +0100

    Update CreateReportScreen.kt

--- app/src/main/java/dk/itu/moapd/x9/mnla_nals/screens/CreateReportScreen.kt
+++ app/src/main/java/dk/itu/moapd/x9/mnla_nals/screens/CreateReportScreen.kt
@@ -122,6 +122,7 @@ fun CreateReportScreen(
                         Report(reportTitle, selectedReportType, reportDescription, reportSeverity)
                     onSubmitReport(report)
                 } else {
+                    // Will be reformatted to use SnackBar in the future
                     Log.d(
                         "Submit", """
                     User report has been submitted with invalid information:
