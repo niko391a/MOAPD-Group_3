3b3c842c24c65f5b48dfed5c5d4c27c25088609f
Nikolaj Alsøe <nals@itu.dk>
2026-04-24 12:18:35 +0200

    Create ReportDetailsScreen.kt

---
+++ app/src/main/java/dk/itu/moapd/x9/mnla_nals/screens/ReportDetailsScreen.kt
@@ -0,0 +1,49 @@
+package dk.itu.moapd.x9.mnla_nals.screens
+
+import android.app.LocaleManager
+import android.os.Build
+import android.os.LocaleList
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.google.firebase.auth.FirebaseUser
+import dk.itu.moapd.x9.mnla_nals.R
+import dk.itu.moapd.x9.mnla_nals.ViewModels.AuthViewModel
+import dk.itu.moapd.x9.mnla_nals.ViewModels.SettingsViewModel
+import dk.itu.moapd.x9.mnla_nals.components.AnimatedColorToggleButton
+import dk.itu.moapd.x9.mnla_nals.components.BasicDropdownMenu
+import dk.itu.moapd.x9.mnla_nals.ui.theme.AppTheme
+import java.util.Locale
+
+@Composable
+fun ReportDetailScreen() {
+
+}
+
+@Composable
+fun MapData() {
+
+}
+
+@Composable
+fun ImageData() {
+
+}
+
+@Composable
+fun ReportData() {
+    
+}
