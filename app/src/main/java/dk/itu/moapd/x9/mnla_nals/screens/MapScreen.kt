2c7c68d09a40f9c17a112dfca1c03b959adf9689
DragonFlyersx <Mathiasl2@hotmail.com>
2026-04-24 12:44:43 +0200

    Update MapScreen.kt

--- app/src/main/java/dk/itu/moapd/x9/mnla_nals/screens/MapScreen.kt
+++ app/src/main/java/dk/itu/moapd/x9/mnla_nals/screens/MapScreen.kt
@@ -1,14 +1,18 @@
 package dk.itu.moapd.x9.mnla_nals.screens
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
@@ -30,22 +34,35 @@ fun MapScreen(
     permissionViewModel: PermissionViewModel = viewModel(),
 
     ) {
+    val context = LocalContext.current
+    val fusedLocationClient = remember {
+        LocationServices.getFusedLocationProviderClient(context)
+    }
 
     PermissionGranter(permissionViewModel)
     val hasPermission = permissionViewModel.requestPermission.collectAsState().value
 
 
     // Itu's coordinates
-    val itu = LatLng(55.6596, 12.5910)
+    var userLocation = LatLng(55.6596, 12.5910)
     // need to show all user reports with markers
     /*
     here Itu cords are hard code can use this and might need the report id asweel as something to show it
     Maybe geofencing
      */
     // update to persons position and show it on the map
-    
+    @SuppressLint("MissingPermission")
+    fusedLocationClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, null)
+        .addOnSuccessListener { location ->
+            if (location != null) {
+                userLocation = LatLng(location.latitude, location.longitude)
+                // Update the camera position to the user's location
+                val cameraPositionState = CameraPosition.fromLatLngZoom(userLocation, 15f)
+            }
+        }
+    // Default to ITU if users Dosent have a location
     val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(itu, 15f)
+        position = CameraPosition.fromLatLngZoom(userLocation, 15f)
     }
 
 
